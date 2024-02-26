d15213f3be317c66bdfcc17ce10ff9aaa4ad2c4f
rayanfelipe260107 <135022313+rayanfelipe260107@users.noreply.github.com>
2024-02-26 09:34:43 -0300

    Comentário

--- app/page.tsx
+++ app/page.tsx
@@ -29,14 +29,15 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
+        {/* <Image
           className={styles.logo}
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
-        />
+        /> */}
+        <h1>Aula Next - 19/02</h1>
       </div>
 
       <div className={styles.grid}>
