d6844b6a0cd2e1a2217d62c90a2db59f2f1b3cd6
rayanfelipe260107 <135022313+rayanfelipe260107@users.noreply.github.com>
2024-03-11 09:48:17 -0300

    Aula de links das telas

---
+++ app/cadusuario/page.tsx
@@ -0,0 +1,8 @@
+export default function Sobre () {
+    return(
+        <>
+            <h1>Cadastro</h1>
+            <p>Insira as Informações sobre o site</p>
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
 export default function Page() {
   return(
     <>
     <h1>Hello, Next.js!</h1>
     <h2>Título 2</h2>
     <h3>Título 3</h3>
+    Qualquer coisa
+    
+
+    <Link href={'/sobre'}>Sobre</Link>
+    <Link href={'/cadusuario'}>Cadastro de Usuário</Link>
     </>
   )
 }
